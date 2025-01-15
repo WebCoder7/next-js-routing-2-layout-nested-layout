3e1d0809c774614be510ad2d987486ce5c7fc47c
WebCoder7 <gulomovgulomjon296@gmail.com>
2025-01-15 13:30:50 +0500

    pushed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import walper from '/public/images/walper.webp';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Trader',
+  description: 'Oyiga 10000 dollor foyda',
+  openGraph: {
+    images: ['/public/images/walper.webp'],
+  },
+};
 
 function About() {
   return (
     <div>
       <h1 className='font-medium text-[20px]'>About Page</h1>
+      <img src={walper.src} alt='img' />
     </div>
   );
 }
